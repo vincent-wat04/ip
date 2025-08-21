638aeb7c985813d39f18db6b43743c7a95aeac37
“Qu <vincent_wat@outlook.com>
2025-08-21 15:36:51 +0800

    Level-0

---
+++ src/main/java/Vince.java
@@ -0,0 +1,10 @@
+public class Vince {
+    public static void main(String[] args) {
+        System.out.println("____________________________________________________________");
+        System.out.println("Hello I'm Vince\n" +
+                "What can I do for you?\n");
+        System.out.println("____________________________________________________________");
+        System.out.println("Bye. Hope to see you again soon!\n");
+        System.out.println("____________________________________________________________");
+    }
+}
