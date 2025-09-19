4ff2675c17fe964558136478787dd83ef4d3050b
“Qu <vincent_wat@outlook.com>
2025-09-19 14:41:03 +0800

    DialogBox: Refactor to use FXML with fx:root construct
    Convert DialogBox to use FXML layout with fx:root pattern.
    - Add @FXML annotations for dialog and displayPicture components
    - Use FXMLLoader with setController and setRoot for fx:root construct
    - Simplify constructor to accept String and Image directly
    - Maintain flip() functionality for bot message alignment
    - Remove manual layout code in favor of FXML definition

--- src/main/java/vince/DialogBox.java
+++ src/main/java/vince/DialogBox.java
@@ -1,76 +1,61 @@
 package vince;
 
-import javafx.geometry.Pos;
-import javafx.scene.control.Label;
-import javafx.scene.image.ImageView;
-import javafx.scene.layout.HBox;
-import javafx.scene.layout.Priority;
-import javafx.scene.layout.Region;
+import java.io.IOException;
+import java.util.Collections;
+
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
-import javafx.geometry.Insets;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.geometry.Pos;
 import javafx.scene.Node;
-import javafx.scene.shape.Rectangle;
+import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.HBox;
 
 /**
- * An example of a custom control using FXML.
- * This control represents a dialog box consisting of an ImageView to represent
- * the speaker's face and a label
- * containing text from the speaker.
+ * Represents a dialog box consisting of an ImageView to represent the speaker's face
+ * and a label containing text from the speaker.
+ * Uses FXML for layout definition with fx:root construct.
  */
 public class DialogBox extends HBox {
-
-    private Label text;
+    @FXML
+    private Label dialog;
+    @FXML
     private ImageView displayPicture;
 
-    public DialogBox(Label l, ImageView iv) {
-        text = l;
-        displayPicture = iv;
+    private DialogBox(String text, Image img) {
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(MainWindow.class.getResource("/view/DialogBox.fxml"));
+            fxmlLoader.setController(this);
+            fxmlLoader.setRoot(this);
+            fxmlLoader.load();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
-        // Enable text wrapping and make it responsive
-        text.setWrapText(true);
-        text.setPrefWidth(Region.USE_COMPUTED_SIZE);
-        text.setMaxWidth(300.0); // Set a reasonable maximum width for text
-        
-        // Make image responsive but with reasonable size constraints
-        displayPicture.setFitWidth(80.0);
-        displayPicture.setFitHeight(80.0);
-        displayPicture.setPreserveRatio(true);
-        displayPicture.setSmooth(true);
-
-        // Set up responsive layout - user dialog should align to right
-        this.setAlignment(Pos.CENTER_RIGHT);
-        this.setSpacing(10.0);
-        this.setPadding(new Insets(5.0));
-        
-        // Add children in order: text first, then image (for user dialog)
-        this.getChildren().addAll(text, displayPicture);
-        
-        // Set preferred width to be responsive but with constraints
-        this.setPrefWidth(Region.USE_COMPUTED_SIZE);
-        this.setMaxWidth(400.0); // Limit the maximum width of the entire dialog box
-        
-        // Ensure the dialog box itself is aligned to the right within its container
-        this.setStyle("-fx-background-color: transparent;");
+        dialog.setText(text);
+        displayPicture.setImage(img);
     }
 
     /**
-     * Flips the dialog box such that the ImageView is on the left and text on the
-     * right. Used for Vince's responses.
+     * Flips the dialog box such that the ImageView is on the left and text on the right.
+     * Used for Vince's responses.
      */
     private void flip() {
-        this.setAlignment(Pos.CENTER_LEFT);
         ObservableList<Node> tmp = FXCollections.observableArrayList(this.getChildren());
-        FXCollections.reverse(tmp);
-        this.getChildren().setAll(tmp);
+        Collections.reverse(tmp);
+        getChildren().setAll(tmp);
+        setAlignment(Pos.CENTER_LEFT);
     }
 
     public static DialogBox getUserDialog(Label l, ImageView iv) {
-        return new DialogBox(l, iv);
+        return new DialogBox(l.getText(), iv.getImage());
     }
 
     public static DialogBox getVinceDialog(Label l, ImageView iv) {
-        var db = new DialogBox(l, iv);
+        var db = new DialogBox(l.getText(), iv.getImage());
         db.flip();
         return db;
     }
